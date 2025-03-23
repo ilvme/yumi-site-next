e99c568163219bb4436248d52064772b8ead4228
kangjia <ikangjia.cn@outlook.com>
2025-03-23 10:19:12 +0800

    good progress

--- src/components/post/PostItem.tsx
+++ src/components/post/PostItem.tsx
@@ -4,11 +4,11 @@ export default async function PostItem({ essay }) {
   return (
     <article
       key={essay.id}
-      className="align-center flex gap-5 p-0.5 transition-all duration-300"
+      className="flex items-center gap-5 p-0.5 transition-all duration-300"
     >
       <span className="text-sm text-gray-400">{essay.publishedAt}</span>
       <Link
-        className="text-gray-500 hover:text-rose-500 hover:underline dark:text-gray-300"
+        className="text-gray-600 hover:text-rose-500 hover:underline dark:text-gray-300"
         href={`/essays/${essay.slug}`}
       >
         {essay.title}
