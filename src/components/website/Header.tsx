910f8ffe599633e569e7b2004f8749b4ee5e256a
kangjia <ikangjia.cn@outlook.com>
2025-03-24 15:28:42 +0800

    so so

--- src/components/website/Header.tsx
+++ src/components/website/Header.tsx
@@ -5,14 +5,14 @@ import Link from 'next/link';
 export default function Header() {
   return (
     <header className="sticky top-0 z-50 w-full border-b border-gray-200 bg-[var(--background)] dark:border-gray-900">
-      <div className="container flex h-16 items-center justify-between">
+      <div className="flex h-14 items-center justify-between">
         <Link href="/" className="flex items-center">
           <span className="text-3xl font-bold">林深时觉寒</span>
         </Link>
-        <nav className="flex items-center space-x-6 text-base font-medium">
+        <nav className="flex items-center space-x-5 text-base font-medium">
           {navItems.map((item) => (
             <Link key={item.href} href={item.href} className="">
-              <span className="text-lg font-bold text-gray-600 hover:text-rose-500 dark:text-[#c7cbd2]">
+              <span className="font-bold text-gray-600 hover:text-rose-500 dark:text-[#c7cbd2]">
                 {item.name}
               </span>
             </Link>
