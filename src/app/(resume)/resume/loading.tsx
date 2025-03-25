3aa396f25a010464359447fbca00c14f44fbc02f
kangjia <ikangjia.cn@outlook.com>
2025-03-25 10:04:11 +0800

    resume loading better

--- src/app/(resume)/resume/loading.tsx
+++ src/app/(resume)/resume/loading.tsx
@@ -1,4 +1,18 @@
+import { Skeleton } from '@/components/ui/skeleton';
+
 export default function Loading() {
-  // You can add any UI inside Loading, including a Skeleton.
-  return <div>简历正在加载中 Loading...</div>;
+  return (
+    <div className="mt-24 flex flex-col gap-5">
+      <Skeleton className="mb-10 h-8 w-80" />
+
+      {Array.from({ length: 3 }).map((_, i) => (
+        <div className="mb-6 space-y-4" key={i}>
+          <Skeleton className="h-6 w-[250px]" />
+          <Skeleton className="h-4 w-[50%]" />
+          <Skeleton className="h-4 w-[60%]" />
+          <Skeleton className="h-4 w-[80%]" />
+        </div>
+      ))}
+    </div>
+  );
 }
