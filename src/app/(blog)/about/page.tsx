2fb49dda0e55787f958836a66d1d4fd2e42e91c1
kangjia <ikangjia.cn@outlook.com>
2025-03-26 16:12:46 +0800

    about me

--- src/app/(blog)/about/page.tsx
+++ src/app/(blog)/about/page.tsx
@@ -12,8 +12,16 @@ export default async function AboutPage() {
         title="关于"
         description="一个脱离不了低级趣味的人的寻常日子里的惨淡经营"
       />
-      一些信息如见主页。
-      <h2 className="mt-4 mb-3 text-2xl font-bold">find me</h2>
+
+      <p>
+        一些信息如见
+        <Link href="/" className="px-0.5 underline">
+          主页
+        </Link>
+        .
+      </p>
+
+      <h2 className="mt-8 mb-3 text-2xl font-bold">find me</h2>
       <div className="flex items-center gap-8">
         <Link
           href="https://github.com/ilvme"
@@ -41,6 +49,8 @@ export default async function AboutPage() {
           <IoMail /> 发邮件
         </Link>
       </div>
+
+      {/*赞助*/}
       <Sponsor />
     </div>
   );
